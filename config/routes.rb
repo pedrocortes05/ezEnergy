8b73f76cb628a1eb959cead30a13839dd03ec8a1
pedrocortes05 <pedrocortespro@gmail.com>
2022-09-24 22:43:09 -0500

    fixed unity route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get "home" => "home#index"
 
-  get "test" => "home#test"
+  get "unity" => "home#unity"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
